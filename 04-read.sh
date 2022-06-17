e34a34347804e9c1b0ffb0dc872521561f372bde
Priyatham32 <87533020+Priyatham32@users.noreply.github.com>
2022-06-17 14:42:33 +0530

    read.sh

---
+++ 04-read.sh
@@ -0,0 +1,7 @@
+read -p 'Enter your name: ' name
+
+echo -e "Hey $name, welcome to devops training..."
+
+read -p 'Enter your phone number: ' phone
+
+echo "Thanks for entering the details, will text you details soon to $phone"
